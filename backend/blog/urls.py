faf45a9eaa8945c462c07bcb771cd651304a031f
Kerimov Ahmed <kerimov.dev@yandex.ru>
2020-11-20 18:04:45 +0300

    Add swagger ui

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-
+from django.views.generic import TemplateView
 from rest_framework import routers
+from rest_framework.schemas import get_schema_view
 from blog.views import UserViewSet, TagViewSet, VoteViewSet, ArticleViewSet, CommentViewSet
 
 router = routers.DefaultRouter()
@@ -11,5 +12,11 @@ router.register('articles', ArticleViewSet)
 router.register('comments', CommentViewSet)
 
 urlpatterns = [
+    path('openapi', get_schema_view(
+        title="Blog",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    path('', TemplateView.as_view(template_name='blog/swagger-ui.html'), name='swagger-ui'),
     path('', include(router.urls)),
 ]
